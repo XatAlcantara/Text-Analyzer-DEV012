a86e94d987d2cdf6f92fa41494ce9a895e714008
Xatzibe Alcantara <143433826+XatAlcantara@users.noreply.github.com>
2023-10-06 08:57:28 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import analyzer from './analyzer.js';
 
-//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
 const boton= document.getElementById("reset-button");
 const cuadrodetexto= document.querySelector("textarea");
 const palabras= document.querySelector("li[data-testid='word-count']");
@@ -10,8 +9,10 @@ const numeros= document.querySelector("li[data-testid='number-count']");
 const suma= document.querySelector("li[data-testid='number-sum']");
 const longmedia= document.querySelector("li[data-testid='word-length-average']");
 
-cuadrodetexto.addEventListener('input', function(){
-  const cuadrodetextoinput = cuadrodetexto.value
+// Selecciona elemntos de DOM por su id o el atributo data-testid y almacena referencias en variables.
+
+cuadrodetexto.addEventListener('input', function(){ // Agrega un evento de escucha al cuadro de texto para el evento input 
+  const cuadrodetextoinput = cuadrodetexto.value // Valor del cuadro de texto
   palabras.innerHTML="Palabras: "+analyzer.getWordCount(cuadrodetextoinput);
   caracteres.innerHTML="Caracteres: "+analyzer.getCharacterCount(cuadrodetextoinput);
   caracteresEX.innerHTML="Caracteres Excl: "+analyzer.getCharacterCountExcludingSpaces(cuadrodetextoinput); 
@@ -19,9 +20,11 @@ cuadrodetexto.addEventListener('input', function(){
   suma.innerHTML="Suma: "+analyzer.getNumberSum(cuadrodetextoinput);
   longmedia.innerHTML="Long. Media: "+analyzer.getAverageWordLength(cuadrodetextoinput);
 });
+// Calcula y muestra la metrica en el cuadro de texto usando los metodos get.
 
-boton.addEventListener("click", function () {
-  cuadrodetexto.value = "";
+
+boton.addEventListener("click", function () { //Agrega un evento de escucha al boton cuando den click.
+  cuadrodetexto.value = ""; //Borra e contenido del cuadro de texto
   palabras.innerHTML = "Palabras: 0";
   caracteres.innerHTML = "Caracteres: 0";
   caracteresEX.innerHTML = "Caracteres Excl: 0";
@@ -29,3 +32,4 @@ boton.addEventListener("click", function () {
   suma.innerHTML = "Suma: 0";
   longmedia.innerHTML = "Long. Media: 0";
 });
+// Restablece las caracteristicas a cero en la interfaz
