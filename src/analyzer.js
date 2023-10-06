d316500c4b58d46b5774b51e6e0918d33282e0f6
XatAlcantara <143433826+XatAlcantara@users.noreply.github.com>
2023-10-05 19:49:50 -0600

    firts proyect

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,21 +1,50 @@
 const analyzer = {  
-  getWordCount: (text) => {
+  getWordCount: (cuadrodetextoinput) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    let words = 0;
+    const wordsArray = cuadrodetextoinput.split(/\s+/);
+    for (let i = 0; i < wordsArray.length; i++) {
+      if (wordsArray[i].trim() !== '') {
+        words++;
+      }
+    }
+    return words;
   },
-  getCharacterCount: (text) => {
+
+  getCharacterCount: (cuadrodetextoinput) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+  const numberchars = cuadrodetextoinput.length; 
+  return numberchars;
   },
-  getCharacterCountExcludingSpaces: (text) => {
+
+  getCharacterCountExcludingSpaces: (cuadrodetextoinput) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    const charsEX = cuadrodetextoinput.replace(/\s+/g, '').replace(/[.,:;_?¡¿!"'()[\]{}«»]/g, '');
+    return charsEX.length;
   },
-  getAverageWordLength: (text) => {    
+
+  getAverageWordLength: (cuadrodetextoinput) => {
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const words = cuadrodetextoinput.trim().split(/\s+/).filter(word => word !== '');
+    let totalLength = 0;
+    for (let i = 0; i < words.length; i++) {
+      totalLength += words[i].length;
+    }
+    const average = words.length > 0 ? totalLength / words.length : 0;
+    return Math.round(average * 100) / 100;
   },
-  getNumberCount: (text) => {
+
+  getNumberCount: (cuadrodetextoinput) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    const numbers = cuadrodetextoinput.split(/\s+/).filter(word => /^\d+(\.\d+)?$/.test(word));
+    return numbers.length;
   },
-  getNumberSum: (text) => {
+  
+  getNumberSum: (cuadrodetextoinput) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    const numbers = cuadrodetextoinput.match(/\d+(\.\d+)?/g) || [];
+    const sum = numbers.reduce((acc, num) => acc + parseFloat(num), 0);
+    return Math.round(sum * 100) / 100;
   },
 };
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+const boton= document.getElementById("reset-button");
+const cuadrodetexto= document.querySelector("textarea");
+const palabras= document.querySelector("li[data-testid='word-count']");
+const caracteres= document.querySelector("li[data-testid='character-count']");
+const caracteresEX= document.querySelector("li[data-testid='character-no-spaces-count']");
+const numeros= document.querySelector("li[data-testid='number-count']");
+const suma= document.querySelector("li[data-testid='number-sum']");
+const longmedia= document.querySelector("li[data-testid='word-length-average']");
+
+cuadrodetexto.addEventListener('input', function(){
+  const cuadrodetextoinput = cuadrodetexto.value
+  palabras.innerHTML="Palabras: "+analyzer.getWordCount(cuadrodetextoinput);
+  caracteres.innerHTML="Caracteres: "+analyzer.getCharacterCount(cuadrodetextoinput);
+  caracteresEX.innerHTML="Caracteres Excl: "+analyzer.getCharacterCountExcludingSpaces(cuadrodetextoinput); 
+  numeros.innerHTML="Numeros: "+analyzer.getNumberCount(cuadrodetextoinput);
+  suma.innerHTML="Suma: "+analyzer.getNumberSum(cuadrodetextoinput);
+  longmedia.innerHTML="Long. Media: "+analyzer.getAverageWordLength(cuadrodetextoinput);
+});
+
+boton.addEventListener("click", function () {
+  cuadrodetexto.value = "";
+  palabras.innerHTML = "Palabras: 0";
+  caracteres.innerHTML = "Caracteres: 0";
+  caracteresEX.innerHTML = "Caracteres Excl: 0";
+  numeros.innerHTML = "Numeros: 0";
+  suma.innerHTML = "Suma: 0";
+  longmedia.innerHTML = "Long. Media: 0";
+});
